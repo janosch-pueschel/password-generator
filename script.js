febcee333eec0e1abbff83e718eeab26f67ee7a3
Janosch Püschel <janosch.pueschel@gmail.com>
2022-12-03 22:49:22 +0100

    updated password generator

--- script.js
+++ script.js
@@ -92,13 +92,42 @@ const characters = [
   "/",
 ];
 
-let firstPassword = Document.getElementById("password-el")
-let randomCharacter = ""
+let firstPassword = document.getElementById("first-password")
+let secondPassword = document.getElementById("second-password")
 
-function generatePassword() { 
-  for (i = 1; i <= 15; i++){
-    randomCharacter += characters[Math.floor(Math.random() * characters.length)]
-  }
-  console.log(randomCharacter)
+
+function generatePasswords() {
+  let randomCharacter = ""
+    for (i = 1; i <= 15; i++){
+      randomCharacter += characters[Math.floor(Math.random() * characters.length)]
+    }
+    firstPassword.textContent = randomCharacter
+
+  let randomPassword = ""
+    for (i = 1; i <= 15; i++){
+        randomPassword += characters[Math.floor(Math.random() * characters.length)]
+      }
+      secondPassword.textContent = randomPassword
 }
-generatePassword()
+
+// let testNumbers = [
+//   "a",
+//   "b",
+//   "c",
+//   "d"
+// ];
+
+// function generatePasswords() {
+//   let randomNumberOne = ""
+//     for (i = 1; i <2; i++){
+//       randomNumberOne += testNumbers[Math.floor(Math.random() * testNumbers.length)]
+//     }
+//     firstPassword.textContent = randomNumberOne
+
+//   let randomNumberTwo = ""
+//     for (i = 1; i <2; i++){
+//         randomNumberTwo += testNumbers[Math.floor(Math.random() * testNumbers.length)]
+//       }
+//       secondPassword.textContent = randomNumberTwo
+// }
+
